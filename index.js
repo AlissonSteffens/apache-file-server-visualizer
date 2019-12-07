0c5b0358ce241bc8bbc7a8906930926d2504e74e
Alisson Steffens <ali.steffens@gmail.com>
2019-12-07 11:24:29 -0300

    :art: add lightbox

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+/ ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
@@ -15,11 +15,14 @@
     // Your code here...
     var list = $("a");
     $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">')
+    $('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.min.css">')
+    $('head').append('<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.min.js" integrity="sha256-CtKylYan+AJuoH8jrMht1+1PMhMqrKnB8K5g012WN5I=" crossorigin="anonymous"></script>')
+
     $('body').append('<div class=""><div class="row" id="list"></div></div>')
     for(var i = 0; i < list.length; i++){
         var link = list[i].getAttribute("href")
         if(link.includes('png') || link.includes('jpg') || link.includes('gif') || link.includes('jpeg') || link.includes('bmp') || link.includes('svg')){
-           $('div#list').append('<div class="card col-md-2" style="margin-bottom: 10px;"><img class="card-img-top" src="'+link+'"><div class="card-body"><p>'+link+'</p></div></div>')
+           $('div#list').append('<div class="card col-md-2" style="margin-bottom: 10px;"><a href="'+link+'" data-lightbox="imgs"><img class="card-img-top" src="'+link+'"><div class="card-body"><p>'+link+'</p></div></a></div>')
         }
 
         if(link.includes('mp4')){
