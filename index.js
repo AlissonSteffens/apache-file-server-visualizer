f03859ea5806954c1d6b31d370e8c933174ccd4a
Rafael <rafaelferreiracosta26@gmail.com>
2019-12-07 02:49:18 -0300

    adding bootstrap, showing items next to each other and mp3 support

--- index.js
+++ index.js
@@ -14,15 +14,20 @@
 
     // Your code here...
     var list = $("a");
+    $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">')
+    $('body').append('<div class=""><div class="row" id="list"></div></div>')
     for(var i = 0; i < list.length; i++){
         var link = list[i].getAttribute("href")
-        $('body').append('<p>'+link+'</p>')
         if(link.includes('png') || link.includes('jpg') || link.includes('gif') || link.includes('jpeg') || link.includes('bmp') || link.includes('svg')){
-           $('body').append('<img src="'+link+'" style="width: 400px; display: block; margin: 10px">')
-        }
-        if(link.includes('mp4')){
-           $('body').append('<video width="320" height="240" controls><source src="'+link+'" type="video/mp4"></video>')
+           $('div#list').append('<div class="card col-md-2" style="margin-bottom: 10px;"><img class="card-img-top" src="'+link+'"><div class="card-body"><p>'+link+'</p></div></div>')
         }
 
+        if(link.includes('mp4')){
+           $('div#list').append('<div class="card col-md-2" style="margin-bottom: 10px;"><video controls><source src="'+link+'" type="video/mp4"></video><div class="card-body"><p>'+link+'</p></div></div>')
+        }
+
+        if(link.includes('mp3')){
+           $('div#list').append('<div class="card col-md-2" style="margin-bottom: 10px;"><audio controls><source src="'+link+'" type="audio/mp3"></audio><div class="card-body"><p>'+link+'</p></div></div>')
+        }
     }
 })();
